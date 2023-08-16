55545c16aecc1f1f6a2d5ad2ddf0e59952db20d6
jerrylovescheese <jerrylovesapple@icloud.com>
2023-08-16 10:52:08 +0800

    Update loader.js

--- static/js/loader.js
+++ static/js/loader.js
@@ -1,16 +1,16 @@
-//显示加载动画
-$(".gif").css("display", "flex");
-//调用ajax发送请求
-esdpec.framework.core.getJsonResult('common/getalarmtree', function (res) {
-    //请求成功后再次隐藏加载动画
-    $(".gif").css("display", "none");
-})
-// setTimeout(() => {
-//     const box = document.getElementById('fa-3x');
+// //显示加载动画
+// $(".gif").css("display", "flex");
+// //调用ajax发送请求
+// esdpec.framework.core.getJsonResult('common/getalarmtree', function (res) {
+//     //请求成功后再次隐藏加载动画
+//     $(".gif").css("display", "none");
+// })
+setTimeout(() => {
+    const box = document.getElementById('fa-3x');
 
-//     // 👇️ removes element from DOM
-//     box.style.display = 'none';
+    // 👇️ removes element from DOM
+    box.style.display = 'none';
 
-//     // 👇️ hides element (still takes up space on page)
-//     // box.style.visibility = 'hidden';
-// }, 1500); // 👈️ time in milliseconds
+    // 👇️ hides element (still takes up space on page)
+    // box.style.visibility = 'hidden';
+}, 1500); // 👈️ time in milliseconds
