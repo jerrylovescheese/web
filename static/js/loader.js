9bbe2d784ee6f5b107af2512f89d4c21d959a4ed
jerrylovescheese <jerrylovesapple@icloud.com>
2023-08-16 10:18:15 +0800

    Update loader.js

--- static/js/loader.js
+++ static/js/loader.js
@@ -13,4 +13,4 @@ setTimeout(() => {
 
     // 👇️ hides element (still takes up space on page)
     // box.style.visibility = 'hidden';
-}, 1300); // 👈️ time in milliseconds
+}, 1500); // 👈️ time in milliseconds
