2797f4f27d221a79ce62dff26bbc96146f375dd4
Jiangyuan Li <jerry@JiangyuandeMacBook-Air.local>
2025-01-18 17:38:23 -0500

    Updated Jan 18 2025

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """
-    Author: Genrative AI
+    Author: Generative AI
     Date: 2025-01-18
-    Version: 1.0
+    Version: 1.1
 """
 
 from flask import Flask, render_template, abort
@@ -31,6 +31,18 @@ def serve_page(page_name):
         abort(404)  # Return a 404 error if the page doesn't exist
 
 
+@app.route("/pages/subpages/<subpage_name>.html")
+def serve_subpage(subpage_name):
+    subpage_path = os.path.join("pages", "subpages", f"{subpage_name}.html")
+    if os.path.exists(subpage_path):
+        # Read the page content here and pass it to the template
+        with open(subpage_path, "r") as file:
+            content = file.read()
+        return render_template("dynamic_subpage.html", content=content)
+    else:
+        abort(404)  # Return a 404 error if the subpage doesn't exist
+
+
 freezer = Freezer(app)
 
 # Register generator for dynamic pages
@@ -46,5 +58,15 @@ def serve_page():
             yield {"page_name": file_name[:-5]}
 
 
-if __name__ == '__main__':
+@freezer.register_generator
+def serve_subpage():
+    # List all files in the 'pages/subpages' directory
+    subpages_dir = os.path.join("pages", "subpages")
+    for file_name in os.listdir(subpages_dir):
+        if file_name.endswith(".html"):
+            # Pass the subpage name without '.html'
+            yield {"subpage_name": file_name[:-5]}
+
+
+if __name__ == "__main__":
     freezer.freeze()
